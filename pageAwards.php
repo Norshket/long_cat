5f2ba3e720d9bec2c2e5146503643901c66544bc
Norshket <74253770+Norshket@users.noreply.github.com>
2021-03-17 18:00:37 +0300

    Add files via upload
    sql

--- pageAwards.php
+++ pageAwards.php
@@ -4,21 +4,23 @@ require_once "./function.php";
 if (!($_GET)) {
     header('Location: /');
 }
-
-
-
-$medal_type = $_GET['medal'];
-$country = $_GET['country'];
 $conn = conn();
+$medal_type = (int)$_GET['medal'];
+
+if($_GET['medal'] !=''){
+  $medal_type = " medal_type_id = ".(int)$_GET['medal']." AND ";
+}
+$country = mysqli_real_escape_string($conn, $_GET['country']);
+
 $sql = "SELECT * FROM country_medals
     JOIN medal_type ON country_medals.medal_type_id = medal_type.id 
     JOIN country ON country_medals.country_id = country.id
     JOIN athletes ON country_medals.athletes_id = athletes.id
     JOIN sport_type ON country_medals.sport_type_id = sport_type.id
-    WHERE medal_type_id = '$medal_type' AND country = '$country'
+    WHERE $medal_type country = '$country'
     ORDER BY medal;
-
     ";
+    
 
 $result = mysqli_query($conn, $sql);
 
