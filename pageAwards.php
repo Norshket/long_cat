6f4dadfed58041253525e355f1d11a07dd6d13c2
pashkov <andreipashkov1@yandex.ru>
2021-03-18 08:29:43 +0300

    изменениня в файле

--- pageAwards.php
+++ pageAwards.php
@@ -10,9 +10,7 @@ $conn = conn();
 if ($_GET['medal']) {
 
     $medal_type = (int)$_GET['medal'];
-    $medal_type = "AND medal_type_id = " . (int)$_GET['medal'] ;
-  
-   
+    $medal_type = "AND medal_type_id = " . (int)$_GET['medal'];
 }
 
 
@@ -23,35 +21,41 @@ $sql = "SELECT * FROM country_medals
         JOIN country ON country_medals.country_id = country.id
         JOIN athletes ON country_medals.athletes_id = athletes.id
         JOIN sport_type ON country_medals.sport_type_id = sport_type.id
-        WHERE  country = '$country' ". $medal_type."
+        WHERE  country = '$country' " . $medal_type . "
         ORDER BY medal_type  
 ";
 
 $result = mysqli_query($conn, $sql);
+while ($row = mysqli_fetch_all($result, MYSQLI_ASSOC)) {
+    $medals_and_people = $row;
+}
+echo'<pre>';
+var_dump($medals_and_people);
+echo'</pre>';
 
-$medals_and_people = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
 
 <?php include './tamplate/header.php' ?>
+
 <body>
 
-<table class="table">
-    <tr>
-        <th class="title_col">Страна </th>
-        <th class="title_col">Вид спорта</th>
-        <th class="title_col">Медаль</th>
-        <th class="title_col">"ФИО" Спортсмена</th>
-    </tr>
-    <?php foreach($medals_and_people as $value)?>
-    <tr>
-        <td class="title_col"><?=$value['country']?></td>
-        <td class="title_col"><?=$value['medal_type']?></td>
-        <td class="title_col"><?=$value['sport_type']?></td>
-        <td class="title_col"><?=$value['name']?> <?=$value['sure_name']?> <?=$value['patronymic']?></td>
-    </tr>
+    <table class="table">
+        <tr>
+            <th class="title_col">Страна </th>
+            <th class="title_col">Вид спорта</th>
+            <th class="title_col">Медаль</th>
+            <th class="title_col">"ФИО" Спортсмена</th>
+        </tr>
+        <?php foreach ($medals_and_people as $value) :?>
+        <tr>
+            <td class="title_col"><?= $value['country'] ?></td>
+            <td class="title_col"><?= $value['sport_type'] ?></td>
+            <td class="title_col"><?= $value['medal_type'] ?></td>
+            <td class="title_col"><?= $value['name'] ?> <?= $value['sure_name'] ?> <?= $value['patronymic'] ?></td>
+        </tr>
+            <?php endforeach;?>
+    </table>
 
-</table>
-    
 
 </body>
 
